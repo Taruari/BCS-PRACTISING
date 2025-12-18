6f685d2fb71023786e43c5142315791d9ccf4585
Taruari <taruari3619r@gmail.com>
2025-12-18 14:52:35 +0530

    Numeric Separator

---
+++ day6/day6.js
@@ -0,0 +1,8 @@
+// null is type of bug (exceptional)
+// any data type convert into number type >>>. NaN
+// undefine * NaN  >>NaN
+// NaN is like a poison  ,it contaminate all the things
+// once become NaN then always NaN
+// learnt BODMAS
+// numeric separator impprove readability
+//
