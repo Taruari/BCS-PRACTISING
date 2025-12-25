aa13f2c5c8120464f41d2c0a09f1678a9dad7656
Taruari <taruari3619r@gmail.com>
2025-12-25 22:54:10 +0530

    Christmas revision completed

---
+++ Holiday-homework/christmas.js
@@ -0,0 +1,51 @@
+// #### 🛠️ Setup Code
+
+// ```js
+// const shelf = ["🍎", "🥪"];
+// const delivery = ["🍫", "🍇"];
+// console.log(mergeInventory(shelf, delivery));
+// ````
+
+// ---
+
+// #### ✅ Output
+
+// ```
+// ["🍎", "🥪", "🍫", "🍇", "🧃"]
+
+// const shelf = ["🍎", "🥪"];
+// const delivery = ["🍫", "🍇"];
+
+// function mergeInventory() {
+//   return [...shelf, ...delivery];
+// }
+
+// console.log(mergeInventory(shelf, delivery));
+// function mergeInventory(shelf = [], delivery = []) {
+//   return [...shelf, ...delivery];
+// }
+
+// const shelf = undefined;
+// const delivery = ["🍫", "🍇"];
+
+// console.log(mergeInventory(shelf, delivery)
+
+// for (let thali = 0; thali <= 10; thali++) {
+//   console.log("🍽️".repeat(thali));
+// }
+// for (let exercise = 0; exercise <= 10; exercise++) {
+//   console.log(`My pushup count is ${exercise}💪`);
+// }
+
+// let sweetNo = 10;
+// while (sweetNo > 0) {
+//   console.log(`${sweetNo} 🍠🍠 `);
+//   sweetNo--;
+// }
+const csk3 = {
+  captain: "MSD",
+  color: "yellow",
+};
+
+// console.log(csk3.stats.win); // Cannot read properties of undefined (reading 'win')
+console.log(csk3.stats.win);
--- practice1/prac9.js
+++ practice1/prac9.js
@@ -84,22 +84,22 @@
 
 // console.log(JSON.stringify(getAllRatings(movies)));
 
-const books = [
-  { title: "Infinite Jest", rating: 4.5, genre: "Fiction" },
-  { title: "The Catcher in the Rye", rating: 3.9, genre: "Fiction" },
-  { title: "Sapiens", rating: 4.9, genre: "History" },
-  { title: "A Brief History of Time", rating: 4.8, genre: "Science" },
-  { title: "Clean Code", rating: 4.7, genre: "Technology" },
-];
+// const books = [
+//   { title: "Infinite Jest", rating: 4.5, genre: "Fiction" },
+//   { title: "The Catcher in the Rye", rating: 3.9, genre: "Fiction" },
+//   { title: "Sapiens", rating: 4.9, genre: "History" },
+//   { title: "A Brief History of Time", rating: 4.8, genre: "Science" },
+//   { title: "Clean Code", rating: 4.7, genre: "Technology" },
+// ];
 
-function getTitles(books, rating) {
-  // Your code
-  let titles = [];
-  for (let book of books) {
-    if (book.rating >= 4.7) {
-      titles.push(book.title);
-    }
-  }
-  return titles;
-}
-console.log(getTitles(books, 4.7));
+// function getTitles(books, rating) {
+//   // Your code
+//   let titles = [];
+//   for (let book of books) {
+//     if (book.rating >= 4.7) {
+//       titles.push(book.title);
+//     }
+//   }
+//   return titles;
+// }
+// console.log(getTitles(books, 4.7));
