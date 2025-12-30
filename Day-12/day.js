7dbd9c785be7a6f7b5eebeb7f5b2fd4a72a0ccbf
Taruari <taruari3619r@gmail.com>
2025-12-30 15:16:57 +0530

    foundation completed

---
+++ Day-12/day.js
@@ -0,0 +1,42 @@
+// const arr = [1, 7, 3, 10, 5];
+// const high = arr.reduce()
+const scores = [
+  {
+    marks: 32,
+    name: "Yvette Merritt",
+  },
+  {
+    marks: 57,
+    name: "Lillian Ellis",
+  },
+  {
+    marks: 22,
+    name: "Mccall Carter",
+  },
+  {
+    marks: 21,
+    name: "Pate Collier",
+  },
+  {
+    marks: 91,
+    name: "Debra Beard",
+  },
+  {
+    marks: 75,
+    name: "Nettie Hancock",
+  },
+  {
+    marks: 20,
+    name: "Hatfield Hodge",
+  },
+];
+// console.log(
+//   scores.reduce((topper, currStudent) =>
+//     currStudent.marks > topper.marks ? currStudent : topper
+//   )
+// );
+
+// Show who go the top mark?
+
+// Output
+// Debra Beard
---
+++ Day-12/day.md
@@ -0,0 +1,12 @@
+filter and map will give array
+which method start with to is immutable method
+even changing the order is mutable
+include >> immutable >>> bolean value
+every original value will not change
+lexiographical order (dictionary order)
+zero is the positive number
+
+reduce method
+it can give any method string or may array
+reduce is immutable
+auto boxing concept >>> learn it from home
