22b364259e499e41a3db890b3a0df50242aed854
Taruari <taruari3619r@gmail.com>
2026-01-17 20:21:21 +0530

    17-01-2026

---
+++ Pracrice/function.js
@@ -0,0 +1,11 @@
+const getRectArea = function (width, height) {
+  return width * height;
+};
+
+console.log(getRectArea(3, 4));
+// Expected output: 12
+
+const x = function (y) {
+  return y * y;
+};
+console.log(x(6));
