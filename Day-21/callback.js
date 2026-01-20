77564bc538a9102e3d77c87fa40371df8a1d9370
Taruari <taruari3619r@gmail.com>
2026-01-20 17:14:33 +0530

    callback

---
+++ Day-21/callback.js
@@ -0,0 +1,14 @@
+//fn1 -callback - when fn takes fn as arg
+//thread --- where process is run
+//run=>cpu=>thread
+//thead count is double the core
+//js is single thread and single asynchronous
+//deadlock is one varible has different values
+//simaltaneously is wrong word in computer science use concarately
+//synchronous means work not smartly
+//js engine work only in call stack
+// setTimeOut  and setInterval is not part of javascript
+//loops are synchronous
+//job of web Apis  is wait
+//EVERY ,FILTER MAP TAKE THREE ARGUMENTS
+//
