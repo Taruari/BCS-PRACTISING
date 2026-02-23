180823b66eafb1bd6e050ee57c9974cad5b457b1
Taruari <taruari3619r@gmail.com>
2026-02-23 13:46:09 +0530

    task python

---
+++ Day-33/01_first.py
@@ -0,0 +1,30 @@
+# full_name = "Tarachand kanwar"
+# print(full_name)
+
+# print(f"welcome, {full_name}")
+
+# age = 20
+# follower = 2_000_000
+
+# print(f"My age is {age} and my followers are {follower * 2}")
+
+# task 2nd
+
+
+# tell= input("Tell me your name?: ")
+# print(tell)
+
+# birth = input("tell me your birth year?: ")
+# print(f"Hi, {tell} you are {2026-int(birth) } years old" )
+
+
+# Clue: Type conversions
+# Task 1.3
+# Output
+# Please provide your Fahrenheit: 98.6
+# The 98.6°F is 37°C
+# (32°F − 32) × 5/9
+
+Fahrenheit = input("tell Fahrenheit?:  ")
+celsius = (float(Fahrenheit) - 32) * 5 / 9
+print(f"{celsius}")
